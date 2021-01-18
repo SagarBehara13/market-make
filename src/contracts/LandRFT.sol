0eb76502eea329955e232f97e3642aa01981a071
Sagar Behara <sagar@cryptocontrol.io>
2021-01-19 02:22:55 +0530

    contracts modifier

--- src/contracts/LandRFT.sol
+++ src/contracts/LandRFT.sol
@@ -5,25 +5,53 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract LandRFT is ERC721{
     
-    ShareToken[] public shareTokens;
+    address owner;
+    uint landCounter = 0;
     
-    constructor()  ERC721("LAND", "LND") public {
-        
+    struct NFTDetails {
+        uint id;
+        string name;
+        string symbol;
+        address admin;
+        uint sqft;
+        uint pricePerSqft;
+        uint monthlyRent;
+        uint image;
     }
     
-    function mint(string memory _name, string memory _symbol, uint _totalSupply, uint _id) public {
+    struct NFTShares {
+        uint id;
+        address owner;
+        uint shares;
+        bool onLoan;
+        address loanedOwner;
+    }
+    
+    ShareToken[] public shareTokens;
+    
+    modifier onlyOwner {
+      require(msg.sender == owner);
+      _;
+   }
+    
+    constructor()  ERC721("LAND", "LND") public {
+        owner = msg.sender;
+    }
+    
+    function _mint(string memory _name, string memory _symbol, uint _totalSupply) public onlyOwner {
+        landCounter++;
         ShareToken _shareToken = new ShareToken(_name, _symbol, _totalSupply);
         shareTokens.push(_shareToken);
         address(_shareToken);
-        
-        _mint(msg.sender, _id);
+  
+        _mint(msg.sender, landCounter);
     }
     
     function getERC20Name(uint id) public view returns(string memory) {
         return shareTokens[id].returnName();
     }
     
-    function approveCrowdSale(uint amount,uint id) public {
+    function approveCrowdSale(uint amount,uint id) public onlyOwner {
         shareTokens[id].approve(address(this), amount);
     }
     
